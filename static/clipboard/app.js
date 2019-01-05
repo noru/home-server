cea39ac62185378c5b6c52326eae8ed7e4835eab
xiuxiuxiu <nooooru@gmail.com>
2019-01-05 18:02:09 +0800

    🚸 handle input debounce stuff

--- static/clipboard/app.js
+++ static/clipboard/app.js
@@ -5,14 +5,16 @@
   }
 
   function createWsClient() {
-    let protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws'
+    let protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:'
     return new WebSocket(protocol + '//' + window.location.host + '/ws/clipboard')
   }
 
   var msg = $('msg'),
       pastearea = $('paste-area'),
       clear = $('clear'),
-      wsClient = createWsClient()
+      wsClient = createWsClient(),
+      editing = false,
+      timeoutId = null
 
   new ClipboardJS('#copy').on('success', function() {
     msg.style = "bottom: 0"
@@ -32,9 +34,16 @@
   wsClient.onopen = function() {
     wsClient.send('get')
   }
-
   pastearea.oninput = function(e) {
-    wsClient.send('set::' + e.target.value)
+    editing = true
+    if (timeoutId !== null) {
+      clearTimeout(timeoutId)
+    }
+    setTimeout(() => {
+      wsClient.send('set::' + e.target.value)
+      timeoutId = null
+      editing = false
+    }, 1000);
   }
 
   clear.onclick = function() {
@@ -42,6 +51,9 @@
     wsClient.send('set::')
   }
 
+  setInterval(() => {
+    !editing && wsClient.send('get')
+  }, 1000);
 
 
 }()
