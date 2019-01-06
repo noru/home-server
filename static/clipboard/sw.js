c1e3979d8db28f2367502ddf4d807724294dcd05
xiuxiuxiu <nooooru@gmail.com>
2019-01-06 15:28:12 +0800

    💄 update icons

--- static/clipboard/sw.js
+++ static/clipboard/sw.js
@@ -6,8 +6,11 @@ var filesToCache = [
   '/static/clipboard/app.js',
   '/static/clipboard/sw.js',
   '/static/clipboard/manifest.json',
-  '/static/clipboard/icon192x192.png',
-  '/static/clipboard/icon512x512.png',
+  '/static/clipboard/favicon.png',
+  '/static/clipboard/icon64.png',
+  '/static/clipboard/icon128.png',
+  '/static/clipboard/icon192.png',
+  '/static/clipboard/icon512.png',
   '//cdn.staticfile.org/clipboard.js/2.0.4/clipboard.min.js',
   '//cdn.jsdelivr.net/npm/clipboard@2/dist/clipboard.min.js',
 ];
@@ -42,7 +45,7 @@ self.addEventListener('fetch', function(event) {
       return response || fetch(event.request);
     }).catch(function (err) {
       // if response not cached and network not available an error is thrown => return fallback image
-      return caches.match('/static/clipboard/icon512x512.png');
+      return caches.match('/static/clipboard/icon512.png');
     })
   )
 });
